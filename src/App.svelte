28651c913f9b67bae9901b0193322c677c7a180d
Irvin Vazquez <irvinvp@gmail.com>
2022-07-01 02:34:54 +0000

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let name = "world";
+</script>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col">
+      <h1>Hello {name}!!</h1>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>

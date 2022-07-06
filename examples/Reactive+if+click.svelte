eb2b74fea0ad1c8d9e2f861fe9d98eba4326d2ea
Irvin Vazquez <irvinvp@gmail.com>
2022-07-06 10:47:38 -0500

    Create Reactive+if+click.svelte

---
+++ examples/Reactive+if+click.svelte
@@ -0,0 +1,74 @@
+<script>
+  let name = "world";
+  let lista = [
+    { serial: 10, speed: 43 },
+    { serial: 11, speed: 0 },
+    { serial: 12, speed: 10 },
+    { serial: 13, speed: 0 },
+  ];
+  let serial_selected = "";
+  $: {
+    if (serial_selected.length > 10) {
+      serial_selected = serial_selected.substring(0, 10);
+      alert("ok");
+    }
+  }
+  let serial_click = "Ninguno";
+  function click(x) {
+    serial_click = x;
+  }
+</script>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col">
+      <h1>Hello {name}!!</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="row">Serial</th>
+            <th>Speed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each lista as item}
+            {#if item.serial == serial_selected || serial_selected == ""}
+              <tr>
+                <th
+                  style="color: {serial_selected == item.serial
+                    ? 'red'
+                    : 'black'};"
+                >
+                  <span
+                    on:click|preventDefault={() => {
+                      click(item.serial);
+                    }}
+                  >
+                    {item.serial}
+                    {serial_selected == item.serial ? "*" : ""}
+                  </span>
+                </th>
+                <td style="color: {item.speed > 0 ? 'red' : 'black'}"
+                  >{item.speed} {item.speed > 0 ? "(overspeed)" : ""}</td
+                >
+              </tr>
+            {/if}
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <input type="text" bind:value={serial_selected} />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <span>Ultimo clic: <strong>{serial_click}</strong></span>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>

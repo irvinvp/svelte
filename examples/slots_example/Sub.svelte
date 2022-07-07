154dbd8a57b64c25316533c77964ee76c65cfc1b
Irvin Vazquez <irvinvp@gmail.com>
2022-07-07 11:35:07 -0500

    Create Sub.svelte

---
+++ examples/slots_example/Sub.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let serial_selected;
+    export let speed_selected;
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        alert("ok");
+    });
+</script>
+
+<span>
+    hola {serial_selected} {speed_selected} 
+</span>
+
+<style>
+
+</style>

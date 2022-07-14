15ce86985ecee8edeaab968f437446fef5d12544
Irvin Vazquez <irvinvp@gmail.com>
2022-07-14 16:45:52 -0500

    Create Sub.svelte

---
+++ examples/localStorage-hostname/Sub.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { test_1, test_2, test_3 } from "./stores.js";
+</script>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col">
+      <h3>Resultado {$test_1}</h3>
+    </div>
+  </div>
+</div>
+
+<style>
+</style>

61e366f884829e7595da63d9573615bdb243c989
Irvin Vazquez <irvinvp@gmail.com>
2022-07-15 14:41:56 -0500

    Create stores.js

---
+++ examples/notifications-worker.stop/stores.js
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+export const test_1 = writable(0);
+export const test_2 = writable("");
+export const test_3 = writable("");
+export const test_4 = writable("");
